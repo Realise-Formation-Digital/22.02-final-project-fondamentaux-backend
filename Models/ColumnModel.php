1a01a9b92bb581569cc7276e403ff956f07336a2
Mougn <boubakars@gmail.com>
2022-05-17 11:59:12 +0200

    modèle_column_fini_BS

--- Models/ColumnModel.php
+++ Models/ColumnModel.php
@@ -2,34 +2,72 @@
 /**/
 require_once __DIR__ . "/Database.php";
 /**/
-class columnsModel extends Database
+class ColumnsModel extends Database
 {
- public $id;
- public $name;
- public $color;
- /**/
- public function getAllColumns()
- {
+    public $id;
+    public $name;
+    public $color;
+    /**/
+    public function getAllColumns($offset = 0, $limit = 10)
+    {
+        //
+        return $this->getMany(
+            "SELECT * FROM columns ORDER BY nom ASC LIMIT $offset, $limit",
+            "ColumnsModel"
+        );
 
- }
- /**/
- public function getSingleColumns()
- {
+    }
+    /**/
+    public function getSingleColumns($id)
+    {
+        //
+        return $this->getSingle(
+            "SELECT * FROM columns WHERE id = $id",
+            "ColumnsModel"
+        );
 
- }
- /**/
- public function insertColumns()
- {
+    }
+    /**/
+    public function insertColumns($array)
+    {
+        //
+        $keys = implode(", ", array_keys($array));
+        $values = implode("', '", array_values($array));
+        //
+        return $this->insert(
+            "INSERT INTO columns ($keys) VALUES ('$values')",
+            "ColumnsModel",
+            "SELECT * FROM columns"
+        );
 
- }
- /**/
- public function updateColumns()
- {
- 
- }
- /**/
- public function deleteColumns()
- {
+    }
+    /**/
+    public function updateColumns($array, $id)
+    {
+        //
+        $values_array = [];
+        foreach($array as $key => $value) {
+            $values_array[] = "$key = '$value'";
+        }
+        $values = implode(",", array_values($values_array));
 
- }
+        //
+        return $this->update(
+            "UPDATE columns SET $values WHERE id = $id",
+            "ColumnsModel",
+            "SELECT id FROM columns WHERE id=$id",
+            "SELECT * FROM columns WHERE id=$id"
+        );
+    
+    }
+    /**/
+    public function deleteColumns($id)
+    {
+        //
+        return $this->delete(
+            "DELETE FROM columns WHERE id=$id",
+            "SELECT id FROM columns WHERE id=$id"
+        );
+
+    }
 }
