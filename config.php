56be0095b80f318642fe262dd4a977fb63bf9aec
Y0N4TH4N <99718249+Y0NAT@users.noreply.github.com>
2022-05-18 22:02:28 +0200

    Delete config.php

--- config.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-  define("DB_HOST", "tg60r.myd.infomaniak.com");
-  define("DB_USERNAME", "tg60r_yonathan");
-  define("DB_PASSWORD", "Moimeme1");
-  define("DB_DATABASE_NAME", "tg60r_yonathan");
