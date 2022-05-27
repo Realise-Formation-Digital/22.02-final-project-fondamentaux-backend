cf0b0ecb7c3d3cdbe12391b6bd87761cf6f5ec33
Marco Tribuzio <marco.tribuz@gmail.com>
2022-05-27 12:15:59 +0200

    ready for production

--- www/config.php
+++ www/config.php
@@ -1,9 +1,15 @@
 <?php
 
-  define("DB_HOST", "maria_db");
-  define("DB_USERNAME", "kanban");
-  define("DB_PASSWORD", "kanban");
+//  define("DB_HOST", "maria_db");
+//  define("DB_USERNAME", "kanban");
+//  define("DB_PASSWORD", "kanban");
+//  define("DB_DATABASE_NAME", "kanban");
+//  define("DB_PORT", "3306");
+
+  define("DB_HOST", "portainer.realise.ch");
+  define("DB_USERNAME", "root");
+  define("DB_PASSWORD", "root");
   define("DB_DATABASE_NAME", "kanban");
-  define("DB_PORT", "3306");
+  define("DB_PORT", "4306");
 
 ?>
